0a7d243c498a2e39405a88268b6c06109d3fdfc6
MACM18 <machathuramadushanka@gmail.com>
2023-05-11 08:26:41 +0530

    First

---
+++ PHP/GetWorkerDetails.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require 'Conn.php';
+$userName = $_POST['userName'];
+$date = $_POST['Date'];
+$response = [];
+$query = "SELECT * FROM operator WHERE User='$userName' AND Date='$date'";
+$result = mysqli_query($con, $query);
+while ($data = mysqli_fetch_array($result)) {
+    $response[] = $data;
+}
+echo json_encode($response);
+?>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: { Green1: "#A4E0A3", Green2: "#0D5B19", Gray1: "#D9D9D9" },
+    },
+  },
+  plugins: [],
+};
