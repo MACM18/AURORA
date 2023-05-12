341267ab6db53d887d9e19cc958dcfb7a41eb25b
MACM18 <machathuramadushanka@gmail.com>
2023-05-12 08:36:41 +0530

    4

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./**/*.{html,js,php}"],
   theme: {
     extend: {
-      colors: { Green1: "#A4E0A3", Green2: "#0D5B19", Gray1: "#D9D9D9" },
+      colors: {
+        Green1: "#A4E0A3",
+        Green2: "#0D5B19",
+        Green3: "#2BAE20",
+        Gray1: "#D9D9D9",
+      },
     },
   },
   plugins: [],
